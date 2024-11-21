1dd363b8c3a9c12a98210d3846150f5508cff074
Félix Boudry <felix.boudry@univ-perp.fr>
2024-11-21 17:23:53 +0100

    Added global PCA. Refacto & env update.

--- functions.R
+++ functions.R
@@ -14,12 +14,14 @@ hclust_plot <- function(clust_data, target) {
 
   clust_data %>%
     dendextend::set("labels_col",
-                    value = c("skyblue", "orange"),
-                    k = 2) %>%
+      value = c("skyblue", "orange"),
+      k = 2
+    ) %>%
     dendextend::set("branches_k_color",
-                    value = c("skyblue", "orange"),
-                    k = 2) %>%
-    dendextend::set("leaves_pch", 15)  %>%
+      value = c("skyblue", "orange"),
+      k = 2
+    ) %>%
+    dendextend::set("leaves_pch", 15) %>%
     dendextend::set("nodes_cex", 0.4) %>%
     dendextend::set("labels_cex", 0.6) %>%
     plot(axes = FALSE)
@@ -44,54 +46,59 @@ draw_confusion_matrix <- function(cm) {
     type = "n",
     xlab = "",
     ylab = "",
-    xaxt = 'n',
-    yaxt = 'n'
+    xaxt = "n",
+    yaxt = "n"
   )
-  title('CONFUSION MATRIX', cex.main = 2)
+  title("CONFUSION MATRIX", cex.main = 2)
 
   # create the matrix
-  rect(150, 430, 240, 370, col = '#3F97D0')
-  text(195, 435, 'Class1', cex = 1.2)
-  rect(250, 430, 340, 370, col = '#F7AD50')
-  text(295, 435, 'Class2', cex = 1.2)
+  rect(150, 430, 240, 370, col = "#3F97D0")
+  text(195, 435, "Class1", cex = 1.2)
+  rect(250, 430, 340, 370, col = "#F7AD50")
+  text(295, 435, "Class2", cex = 1.2)
   text(125,
-       370,
-       'Predicted',
-       cex = 1.3,
-       srt = 90,
-       font = 2)
-  text(245, 450, 'Actual', cex = 1.3, font = 2)
-  rect(150, 305, 240, 365, col = '#F7AD50')
-  rect(250, 305, 340, 365, col = '#3F97D0')
-  text(140, 400, 'Class1', cex = 1.2, srt = 90)
-  text(140, 335, 'Class2', cex = 1.2, srt = 90)
+    370,
+    "Predicted",
+    cex = 1.3,
+    srt = 90,
+    font = 2
+  )
+  text(245, 450, "Actual", cex = 1.3, font = 2)
+  rect(150, 305, 240, 365, col = "#F7AD50")
+  rect(250, 305, 340, 365, col = "#3F97D0")
+  text(140, 400, "Class1", cex = 1.2, srt = 90)
+  text(140, 335, "Class2", cex = 1.2, srt = 90)
 
   # add in the cm results
   res <- as.numeric(cm$table)
   text(195,
-       400,
-       res[1],
-       cex = 1.6,
-       font = 2,
-       col = 'white')
+    400,
+    res[1],
+    cex = 1.6,
+    font = 2,
+    col = "white"
+  )
   text(195,
-       335,
-       res[2],
-       cex = 1.6,
-       font = 2,
-       col = 'white')
+    335,
+    res[2],
+    cex = 1.6,
+    font = 2,
+    col = "white"
+  )
   text(295,
-       400,
-       res[3],
-       cex = 1.6,
-       font = 2,
-       col = 'white')
+    400,
+    res[3],
+    cex = 1.6,
+    font = 2,
+    col = "white"
+  )
   text(295,
-       335,
-       res[4],
-       cex = 1.6,
-       font = 2,
-       col = 'white')
+    335,
+    res[4],
+    cex = 1.6,
+    font = 2,
+    col = "white"
+  )
 
   # add in the specifics
   plot(
@@ -101,8 +108,8 @@ draw_confusion_matrix <- function(cm) {
     xlab = "",
     ylab = "",
     main = "DETAILS",
-    xaxt = 'n',
-    yaxt = 'n'
+    xaxt = "n",
+    yaxt = "n"
   )
   text(10, 85, names(cm$byClass[1]), cex = 1.2, font = 2)
   text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex = 1.2)
